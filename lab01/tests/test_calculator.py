f43aefdc23f2b387e8dc004caa573a582f6e0baf
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2025-02-28 09:19:28 +0100

    zad1

---
+++ lab01/tests/test_calculator.py
@@ -0,0 +1,24 @@
+import unittest
+from src.calculator import Calculator
+
+
+class TestCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calculator = Calculator()
+
+    def test_check_sum_int(self):
+        self.assertEqual(self.calculator.add(4, 5), 9)
+
+    def test_check_sum_float(self):
+        self.assertAlmostEqual(self.calculator.add(0.1, 1e23), 1e23)
+
+    def test_check_sum_string(self):
+        self.assertEqual(self.calculator.add("aa", "bb"), "aabb")
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()

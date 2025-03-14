6460a34e16900638c181bd43b441dd70b54882f0
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-03-14 09:10:40 +0100

    etap6

--- lab03/src/trip.py
+++ lab03/src/trip.py
@@ -1,11 +1,18 @@
 class Trip:
 
     def __init__(self, destination, duration):
+        self.participants = []
         self.destination = destination
         self.duration = duration
 
     def calculate_cost(self):
         return 100 * self.duration
+
+    def add_participant(self, param):
+        if param == "":
+            raise ValueError("Participant name cannot be empty")
+
+        self.participants.append(param)
 
 
 
--- lab03/tests/test_trip.py
+++ lab03/tests/test_trip.py
@@ -11,17 +11,32 @@ class TestTripInitialization(unittest.TestCase):
         trip1 = Trip("Paris", 7)
         self.assertIsInstance(trip1, Trip)
 
-    def test_initilization(self):
+    def test_initialization(self):
         trip1 = Trip("Paris", 7)
         self.assertEqual(trip1.destination, "Paris")
         self.assertEqual(trip1.duration, 7)
 
     def test_calculate_cost(self):
         trip1 = Trip("Paris", 7)
-        self.assertEqual(trip1.calculate_cost(),700)
+        self.assertEqual(trip1.calculate_cost(), 700)
         trip2 = Trip("Rome", 5)
         self.assertEqual(trip2.calculate_cost(), 500)
 
+    def test_add_participant(self):
+        trip1 = Trip("Paris", 7)
+        trip1.add_participant("John")
+        self.assertIn("John", trip1.participants)
+        trip1.add_participant("Alice")
+        self.assertIn("Alice", trip1.participants)
+        trip1.add_participant("Bob")
+        self.assertIn("Bob", trip1.participants)
+
+    def test_add_empty_participant(self):
+        trip1 = Trip("Paris", 7)
+        with self.assertRaises(ValueError) as context:
+            trip1.add_participant("")
+
+        self.assertEqual(str(context.exception), "Participant name cannot be empty")
 
     def tearDown(self):
         pass

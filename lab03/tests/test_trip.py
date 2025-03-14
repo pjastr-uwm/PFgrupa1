23f936b63c33179a2564809f135b4a34962b4fc9
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-03-14 08:19:54 +0100

    lab03 - etap1

---
+++ lab03/tests/test_trip.py
@@ -0,0 +1,19 @@
+import unittest
+from src.trip import Trip
+
+class TestTripInitialization(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_initilization(self):
+        trip1 = Trip("Paris", 7)
+        self.assertEqual(trip1.desitination, "Paris")
+        self.assertEqual(trip1.duration, 7)
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()

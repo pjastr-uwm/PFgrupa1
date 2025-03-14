fb1546136554a2853feea403ff6c0c34d8744b71
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-03-14 08:25:09 +0100

    Update test_trip.py

--- lab03/tests/test_trip.py
+++ lab03/tests/test_trip.py
@@ -1,10 +1,15 @@
 import unittest
 from src.trip import Trip
 
+
 class TestTripInitialization(unittest.TestCase):
 
     def setUp(self):
         pass
+
+    def test_creation(self):
+        trip1 = Trip("Paris", 7)
+        self.assertIsInstance(trip1, Trip)
 
     def test_initilization(self):
         trip1 = Trip("Paris", 7)

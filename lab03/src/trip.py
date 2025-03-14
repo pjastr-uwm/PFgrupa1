f3ffbea410f20d3d79140ab808eb3185e9d242d1
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-03-14 08:49:05 +0100

    etap 4

--- lab03/src/trip.py
+++ lab03/src/trip.py
@@ -3,3 +3,9 @@ class Trip:
     def __init__(self, destination, duration):
         self.destination = destination
         self.duration = duration
+
+    def calculate_cost(self):
+        return 100 * self.duration
+
+
+
--- lab03/tests/test_trip.py
+++ lab03/tests/test_trip.py
@@ -16,6 +16,13 @@ class TestTripInitialization(unittest.TestCase):
         self.assertEqual(trip1.destination, "Paris")
         self.assertEqual(trip1.duration, 7)
 
+    def test_calculate_cost(self):
+        trip1 = Trip("Paris", 7)
+        self.assertEqual(trip1.calculate_cost(),700)
+        trip2 = Trip("Rome", 5)
+        self.assertEqual(trip2.calculate_cost(), 500)
+
+
     def tearDown(self):
         pass
 

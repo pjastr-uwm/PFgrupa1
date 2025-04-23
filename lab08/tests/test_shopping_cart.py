8b37381c8950941f54d52206463023b97c302d0e
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-04-23 09:24:36 +0200

    Update test_shopping_cart.py

--- lab08/tests/test_shopping_cart.py
+++ lab08/tests/test_shopping_cart.py
@@ -13,6 +13,13 @@ def decimal_product():
 def empty_cart():
     return ShoppingCart()
 
+@pytest.fixture
+def filled_card(empty_cart, decimal_product):
+    for _ in range(5):
+        empty_cart.add_product(decimal_product)
+
+    return empty_cart
+
 def test_adding(empty_cart, sample_product):
     empty_cart.add_product(sample_product)
     assert sample_product.id in empty_cart.products.keys()
@@ -27,5 +34,26 @@ def test_adding_decimal_product(empty_cart,decimal_product):
 def test_multiple_adding(empty_cart, sample_product, quantity):
     empty_cart.add_product(sample_product, quantity)
     assert empty_cart.get_product_count() == quantity
+
+def test_removing_invalid(filled_card, sample_product):
+    with pytest.raises(ValueError):
+        filled_card.remove_product(sample_product.id)
+
+def test_removing(filled_card, decimal_product):
+    filled_card.remove_product(decimal_product.id)
+    assert filled_card.get_product_count() == 4
+
+
+def test_clear(filled_card, decimal_product):
+    while filled_card.get_product_count() > 0:
+        filled_card.remove_product(decimal_product.id)
+
+    assert filled_card.get_product_count() == 0
+
+
+def test_clear_second(filled_card, decimal_product):
+    filled_card.remove_product(decimal_product.id, 7)
+
+    assert filled_card.get_product_count() == 0
 
 
